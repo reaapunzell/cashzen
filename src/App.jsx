243c491a5f595b7f9ff39d0a5d370749cc975c45
Rea Maroga <35115923+reaapunzell@users.noreply.github.com>
2024-10-28 04:28:15 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,56 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import AddTransaction from "./components/AddTransactions";
 import TransactionList from "./components/TransactionList";
-import { Link } from "react-router-dom";
-import { Navbar, Nav, Container } from 'react-bootstrap';
 import WelcomePage from "./components/Home";
-import { useNavigate } from "react-router-dom"
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
+// Navbar Component
 const MyNavbar = ({ userId }) => {
-    const navigate = useNavigate(); 
-  
-    const handleDashboardClick = () => {
-      navigate(`/transactions/${userId}`); 
-    };
-  
-    const handleAddTransactionClick = () => {
-      navigate("/add-transaction"); 
-    };
-  
-    return (
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="/">Cashzen</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link onClick={handleDashboardClick}>Dashboard</Nav.Link>
-              <Nav.Link onClick={handleAddTransactionClick}>Transactions</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  const navigate = useNavigate();
+
+  const handleDashboardClick = () => {
+    navigate(`/transactions/${userId}`); 
   };
 
-  function App() {
-  
-    return (
-      <Router>
-        <MyNavbar userId={userId} /> {}
-        <div>
-          <Routes>
-            <Route path="/" element={<WelcomePage />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/add-transaction" element={<AddTransaction />} />
-            <Route path="/transactions/:userId" element={<TransactionList />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Cashzen</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={handleDashboardClick}>Dashboard</Nav.Link>
+            <Nav.Link as={Link} to="/add-transaction">Add Transactions</Nav.Link>
+            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+function App() {
+ 
+  const userId = localStorage.getItem("userId");
+
+  return (
+    <Router>
+      <MyNavbar userId={userId} /> {}
+      <div>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/add-transaction" element={<AddTransaction />} />
+          <Route path="/transactions/:userId" element={<TransactionList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
